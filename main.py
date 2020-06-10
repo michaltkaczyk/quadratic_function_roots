ce820c0945ca3b170a88cb1480cc3859bf344617
michaltkaczyk <michaltkaczyk@outlook.com>
2020-06-10 13:01:42 +0200

    Validate the a coefficient
    Validate the "a" coefficient to make sure it is not equal to zero

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ def read_coefficient(coefficient_name):
 
 
 a = read_coefficient("a")
+
+if a == 0:
+    print("The a coefficient cannot be equal to zero")
+    sys.exit(1)
+
 b = read_coefficient("b")
 c = read_coefficient("c")
 
 print("Standard form: f(x) =", "{:+.4f}".format(a), "x^2", "{:+.4f}".format(b), "x", "{:+.4f}".format(c))
 
-# TODO: check if a not 0
 # TODO: space clean up in the functional form
 
 delta = b ** 2 - 4 * a * c
@@ -33,7 +37,6 @@ if delta >= 0:
         r1 = (-b - math.sqrt(delta)) / (2 * a)
         r2 = (-b + math.sqrt(delta)) / (2 * a)
         print("Roots of this quadratic function are:", "{:+.4f}".format(r1), "and", "{:+.4f}".format(r2))
-        print("Factored form: f(x) =", "{:+.4f}".format(a), "(x", "{:+.4f}".format(r1),")(x", "{:+.4f}".format(r2), ")")
     else:
         r1 = -b / (2 * a)
         print("The only root of this quadratic function is:", "{:+.4f}".format(r1))
