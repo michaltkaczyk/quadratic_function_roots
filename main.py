54b171baed0d14947706ae52b53d111a2fa94bf4
michaltkaczyk <michaltkaczyk@outlook.com>
2020-06-26 22:27:35 +0200

    Algebraic solver added
    Added an algebraic solver based on the sympy library

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import math
+from sympy import Symbol, solve
 
 
 def read_coefficient(coefficient_name):
@@ -14,7 +15,8 @@ def format_coefficient(coefficient_value):
     return "{:+.4f}".format(coefficient_value).replace("+", "+ ").replace("-", "- ")
 
 
-def show_roots(a, b, c):
+def show_roots_numeric(a, b, c):
+    print("\nCalculating roots of a quadratic function using generic Python approach")
     print("Standard form: f(x) = ", format_coefficient(a), " x^2 ", format_coefficient(b),
           " x ", format_coefficient(c), sep="")
 
@@ -38,7 +40,25 @@ def show_roots(a, b, c):
             print("Factored form: f(x) = ", format_coefficient(a), " (x ", format_coefficient(-p), ")^2", sep="")
             print("The only root of this quadratic function is:", "{:-.4f}".format(p))
     else:
-        print("This quadratic function has no roots")
+        print("This quadratic function has no real roots")
+
+
+def show_roots_symbolic(a, b, c):
+    print("\nCalculating roots of a quadratic function using the SymPy library")
+
+    x = Symbol("x")
+    f = a * (x ** 2) + b * x + c
+    roots = solve(f)
+
+    is_root_real = list(map(lambda x: x.is_real, roots))
+    real_roots = [root for (root, flag) in zip(roots, is_root_real) if flag]
+
+    if len(real_roots) == 2:
+        print("Roots of this quadratic function are:", real_roots[0], "and", real_roots[1])
+    elif len(real_roots) == 1:
+        print("The only root of this quadratic function is:", real_roots[0])
+    else:
+        print("This quadratic function has no real roots")
 
 
 if __name__ == '__main__':
@@ -51,4 +71,5 @@ if __name__ == '__main__':
     b = read_coefficient("b")
     c = read_coefficient("c")
 
-    show_roots(a, b, c)
+    show_roots_numeric(a, b, c)
+    show_roots_symbolic(a, b, c)
